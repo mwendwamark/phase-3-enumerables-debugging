d67804dba26c6a516076f371a9d47472c7216d91
mark mwendwa <mwendwamark310@gmail.com>
2023-05-16 08:30:40 +0300

    completed assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require "pry"
+def reverse_each_word(sentence)
+   words = sentence.split
+   reversed_words = []
+
+  words.map do |word|
+   reversed_words << word.reverse
+  end
+ 
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word("Does it work?")
+puts reverse_each_word(" I think it does work")
+puts reverse_each_word("My name is Mark and he is Dennis")
+
+# def reverse_each_word(sentence)
+#   sentence.split.map(&:reverse).join(" ")
+# end
+# this is the other way around to write that code above
